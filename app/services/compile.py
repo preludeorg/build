94fcc80e73b35dcb94e9a33d79cd69a808a8ddf1
david <49954156+privateducky@users.noreply.github.com>
2022-09-14 10:10:06 -0400

    Update compile.py

--- app/services/compile.py
+++ app/services/compile.py
@@ -4,7 +4,7 @@ from vertebrae.service import Service
 class CompileService(Service):
 
     def __init__(self):
-        self.log = self.logger('testbench')
+        self.log = self.logger('compile')
 
     async def compile(self, name: str) -> str:
         """ Compile a DCF """
