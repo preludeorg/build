3c41e8f4e1e2126ca372100b879118514ed7e712
makk94 <mahina.kaholokula@prelude.org>
2022-09-19 15:38:48 -0700

    add compile service

--- backend/services/compile.py
+++ backend/services/compile.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+import sys
+
 from vertebrae.service import Service
 
 
@@ -9,6 +13,29 @@ class CompileService(Service):
 
     async def compile(self, account_id: str, name: str) -> str:
         """ Compile a DCF """
-        code = await self.file.read(f'{self.file.name}/{account_id}/src/{name}')
-        # TODO: 1) compile 2) return compiled code
-        return code
+        targets = dict(
+            linux_amd64='x86_64-linux-gnu',
+            linux_arm64='aarch64-linux-gnu',
+            darwin_amd64='x86_64-macos',
+            darwin_arm64='aarch64-macos',
+            windows_amd64='x86_64-windows',
+            windows_arm64='aarch64-windows',
+        )
+        src = os.path.join(self.database.directory.name, 'src', f'{name}.c')
+        dst = os.path.join(self.database.directory.name, 'dst', f'{name}')
+        target = targets.get(name.split('_').pop().replace('-', '_'))
+        subprocess.call([
+            sys.executable,
+            '-m',
+            'ziglang',
+            'build-lib',
+            src,
+            '-fPIC',
+            '-dynamic',
+            '--library',
+            'c',
+            '-target',
+            f'{target}',
+            f'-femit-bin={dst}'
+        ])
+        return dst
