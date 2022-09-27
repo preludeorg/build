2a206582ebfe6c213c0e228b8dc145910600ccbe
Francis Mireault <mireaulf@gmail.com>
2022-09-26 20:26:03 -0400

    Sign a dcf

--- backend/services/signing.py
+++ backend/services/signing.py
@@ -1,3 +1,6 @@
+import hashlib
+
+from vertebrae.config import Config
 from vertebrae.service import Service
 
 
@@ -5,7 +8,16 @@ class SigningService(Service):
 
     def __init__(self, name):
         super().__init__(name)
+        self.s3 = self.db().s3
 
-    async def sign(self, binary) -> str:
+    @staticmethod
+    def __compute_digest(data: bytes) -> str:
+        h = hashlib.sha3_512()
+        h.update(data)
+        return h.hexdigest()
+
+    async def sign(self, account_id: str, dcf_name: str, binary: bytes) -> str:
         """ Sign a compiled DCF """
-        # TODO: 1) sign file 2) upload to S3
+        digest = self.__compute_digest(binary)
+        self.s3.write(Config.find('aws')['buckets']['accounts'], f'{account_id}/{dcf_name}', f'{digest}{str(binary)}')
+        return digest
