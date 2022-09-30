15825a1acea4f5e8db32bdd32de9367b9150ecba
Francis Mireault <mireaulf@gmail.com>
2022-09-30 14:19:36 -0400

    Copy assets to dist

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
- entry: {
-  index: './client/js/main.js'
- },
- plugins: [
-   new HtmlWebpackPlugin({
-       title: 'Operator',
-       template: 'client/templates/index.html',
-   }),
- ],
- output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
- },
+    mode: 'development',
+    entry: {
+        index: './client/js/main.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'client/templates/index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "client/assets", to: "static/assets" },
+            ],
+        }),
+    ],
+    output: {
+        filename: 'static/[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
 };
