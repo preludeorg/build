4c922e4e051d5fe3751d814229fab3395d67e846
Francis Mireault <mireaulf@gmail.com>
2022-09-30 15:26:08 -0400

    Move js to static/js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 { from: "client/assets", to: "static/assets" },
                 { from: "client/css/font", to: "static/css/font" },
                 { from: "client/css/*.css", to: "static/css/[name][ext]" },
-                { from: "client/js/lib", to: "client/js/lib" }
+                { from: "client/js/lib", to: "static/js/lib" }
             ],
         }),
     ],
