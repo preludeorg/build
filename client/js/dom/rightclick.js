e1fd9acc4794b49fe02eff71050530b0f7a10190
privateducky <david@prelude.org>
2022-10-02 12:35:58 -0400

    fixing classification issue

--- client/js/dom/rightclick.js
+++ client/js/dom/rightclick.js
@@ -21,8 +21,7 @@ let RightClick = {
                                 .removeClass('context-menu-active');
                             menu.find(`li.context-menu-item.context-menu-icon.context-menu-icon-${key.substring(key.indexOf('!') + 1)}`)
                                 .addClass('context-menu-active');
-                            $(`${options.selector}`).find('.ttp-classification')
-                                .attr("src", `/client/assets/classifications/${classification}.svg`);
+                            $(this).find('.ttp-classification').attr("src", `/client/assets/classifications/${classification}.svg`);
                         });
                     } else {
                         const name = `${Page.id}_${key}`;
