ac6b1d9ab8f5e912236c9058f0266acd39d592c7
Stephan Wampouille <stephanwampouille@Stephans-MacBook-Air.local>
2022-10-11 21:38:14 -0400

    WIP Adding custom/default server options

--- client/js/plugins/server.js
+++ client/js/plugins/server.js
@@ -13,10 +13,31 @@ class Server {
             'You can host alternative Server instances and log in below.' +
             `You are logged into ${creds.host} as ${creds.account}.`);
 
+        contents.append($('<div id="server-selection">'))
+        $('#server-selection').append($('<div id="default-server" class="active-server">').text("Default"));
+        $('#server-selection').append($('<div id="custom-server">').text("Custom"));
+
+        contents.append($('<div id="default-container">'));
+        contents.append($('<div id="custom-container">'));
+
+        $('#default-container').append($('<div id="default-submit" class="plugin-button">').text('Connect to Prelude server')).on('click', (ev) => {
+            const host = '';
+            const account = '';
+            const token = '';
+            Api.ping(host, account, token).then(() => {
+                Api.setCredentials(host, account, token);
+                location.reload();
+            }).catch(err => {
+               console.error(`Unable to connect to ${host}, ${err}`);
+            }).finally(() => {
+               $('#spinner').hide();
+            });
+        });
+
         const ip = $('<input id="i-ip" class="plugin-input" placeholder="Enter an IP" spellcheck="false">');
-        const account = $('<input id="i-account" class="plugin-input" placeholder="Enter account ID" type="text" spellcheck="false">');
+        const account = $('<input id="i-account" class="plugin-input" placeholder="Enter a username" type="text" spellcheck="false">');
         const token = $('<input id="i-token" class="plugin-input" placeholder="Enter a token" type="password">');
-        const submit = $('<button class="plugin-button">').text('Login').on('click', (ev) => {
+        const submit = $('<button class="plugin-button">').text('Connect to custom server').on('click', (ev) => {
             ev.stopPropagation();
             ev.preventDefault();
 
@@ -33,10 +54,24 @@ class Server {
                $('#spinner').hide();
             });
         });
-        contents.append(ip);
-        contents.append(account);
-        contents.append(token);
-        contents.append(submit);
+        $('#custom-container').append(ip);
+        $('#custom-container').append(account);
+        $('#custom-container').append(token);
+        $('#custom-container').append(submit);
+
+        $("#default-server").on('click', (ev) => {
+            $('#custom-container').hide();
+            $('#default-container').show();
+            $('#default-server').toggleClass('active-server')
+            $('#custom-server').toggleClass('active-server')
+        })
+
+        $("#custom-server").on('click', (ev) => {
+            $('#custom-container').show();
+            $('#default-container').hide();
+            $('#default-server').toggleClass('active-server')
+            $('#custom-server').toggleClass('active-server')
+        })
     }
 }
 
