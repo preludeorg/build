ebd4cb721da78c96499e30f81ee597dfa7ee2304
david <49954156+privateducky@users.noreply.github.com>
2022-10-14 06:44:26 -0400

    dropping tags (#51)

--- client/js/dom/rightclick.js
+++ client/js/dom/rightclick.js
@@ -13,15 +13,6 @@ let openContextMenu = (selector, trigger='right') => {
                 });
             } else if (key === 'rename') {
                 $(this).find('#ttp-name').attr('contentEditable', 'true').focus();
-            } else if (key.startsWith('!')) {
-                const tags = key.substring(key.indexOf('!') + 1);
-                Api.ttp.save({id: Page.id, tags: [tags]}).then(() => {
-                    let menu = $('ul.context-menu-list');
-                    menu.find(`li.context-menu-item.context-menu-icon`)
-                        .removeClass('context-menu-active');
-                    menu.find(`li.context-menu-item.context-menu-icon.context-menu-icon-${key.substring(key.indexOf('!') + 1)}`)
-                        .addClass('context-menu-active');
-                });
             } else {
                 const name = `${Page.id}_${key}`;
                 const ext = key.split('.').pop();
@@ -36,31 +27,22 @@ let openContextMenu = (selector, trigger='right') => {
         },
         items: {
             fold1: {
-                name: "Assign endpoint tags",
-                items: {
-                    '!server': {name: 'server', icon: 'unknown'},
-                    '!workstation': {name: 'workstation', icon: 'unknown'},
-                    '!container': {name: 'container', icon: 'unknown'},
-                }
-            },
-            sep1: '---------',
-            fold2: {
-                name: "Attach Linux test",
+                name: "New Linux test",
                 items: {
                     'linux.c': {name: 'linux.c', icon: 'linux'},
                     'linux.cs': {name: 'linux.cs', icon: 'linux'},
                     'linux.swift': {name: 'linux.swift', icon: 'linux'}
                 }
             },
-            fold3: {
-                name: "Attach MacOS test",
+            fold2: {
+                name: "New MacOS test",
                 items: {
                     'darwin.c': {name: 'darwin.c', icon: 'apple'},
                     'darwin.cs': {name: 'darwin.cs', icon: 'apple'},
                     'darwin.swift': {name: 'darwin.swift', icon: 'apple'}
                 }
             },
-            sep2: '---------',
+            sep1: '---------',
             rename: {name: 'Rename', icon: 'edit'},
             delete: {name: 'Delete', icon: 'delete'}
         }
