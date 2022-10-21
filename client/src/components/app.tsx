a33cd595ba46940c7cc915419d4ae668788780f2
Kenrick Beckett <kenrick.beckett@gmail.com>
2022-10-20 19:15:25 -0600

    Fix active nav bar

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -6,7 +6,7 @@ import EditorWindow from "./editor/window";
 import Terminal from "./terminal/terminal";
 import Navbar from "./navbar/navbar";
 import Welcome from "./welcome/welcome";
-import EditorIntro from "./editor/editorIntro";
+import EditorIntro from "./editor/editor-intro";
 
 function App() {
   const editor = useEditor();
@@ -26,22 +26,25 @@ function App() {
       <main>
         <section>
           {navigation === "welcome" && <Welcome />}
-          {!editor.hasTabs && navigation === "editor" && <EditorIntro />}
-          {editor.hasTabs && (
-            <EditorWindow
-              tabs={editor.tabs}
-              buffer={editor.buffer}
-              currentTab={editor.currentTab}
-              closeTab={editor.closeTab}
-              switchTab={editor.switchTab}
-              updateBuffer={editor.updateCurrentBuffer}
-              setNavigation={setNavigation}
-            />
-          )}
+          {navigation === "editor" &&
+            (editor.hasTabs ? (
+              <EditorWindow
+                tabs={editor.tabs}
+                buffer={editor.buffer}
+                currentTab={editor.currentTab}
+                closeTab={editor.closeTab}
+                switchTab={editor.switchTab}
+                updateBuffer={editor.updateCurrentBuffer}
+                setNavigation={setNavigation}
+              />
+            ) : (
+              <EditorIntro />
+            ))}
         </section>
         <footer>
           <Terminal
             openTab={() => {
+              setNavigation("editor");
               editor.openTab({
                 name: `linux-${Date.now()}-x84.swift`,
                 code: new Swift().bootstrap(),
