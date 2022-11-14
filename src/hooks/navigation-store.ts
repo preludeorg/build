11ee2e81b372b6e685dbf040843f2e4c643729bc
Kenrick Beckett <kenrick.beckett@gmail.com>
2022-11-14 16:53:56 -0700

    move beforeinstallprompt logic to store because it only runs once

--- src/hooks/navigation-store.ts
+++ src/hooks/navigation-store.ts
@@ -1,10 +1,16 @@
 import create from "zustand";
 
+interface BeforeInstallEvent {
+  prompt(): void;
+  userChoice?: Promise<{ outcome: "accepted" }>;
+}
 interface NavigationStore {
+  installer?: BeforeInstallEvent;
   panel: string;
   serverPanelVisible: boolean;
   navigate: (panel: string) => void;
   toggleServerPanel: () => void;
+  setInstaller: (event?: unknown) => void;
 }
 
 const useNavigationStore = create<NavigationStore>((set) => ({
@@ -15,6 +21,9 @@ const useNavigationStore = create<NavigationStore>((set) => ({
   },
   toggleServerPanel() {
     set((state) => ({ serverPanelVisible: !state.serverPanelVisible }));
+  },
+  setInstaller(installer) {
+    set(() => ({ installer: installer as BeforeInstallEvent }));
   },
 }));
 
