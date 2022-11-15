30700dbfef64a7e183ab23cd1d440c0ff165bd97
Kenrick Beckett <760470+kenrick@users.noreply.github.com>
2022-11-15 15:06:36 -0700

    add building variant (#130)

--- src/components/editor/control-panel.tsx
+++ src/components/editor/control-panel.tsx
@@ -1,67 +1,56 @@
 import PlayIcon from "../icons/play-icon";
-import ChevronIcon from "../icons/chevron-icon";
-import HelpIcon from "../icons/help-icon";
-import CopyIcon from "../icons/copy-icon";
+
 import styles from "./control-panel.module.css";
 import useEditorStore from "../../hooks/editor-store";
-import { useState } from "react";
-import classNames from "classnames";
-import VariantIcon from "../icons/variant-icon";
-import { parseVariant } from "../../lib/utils/parse-variant";
 import { validate } from "../../lib/lang/linter";
 import { getLanguage } from "../../lib/lang";
+import useTerminalStore from "../../hooks/terminal-store";
+import shallow from "zustand/shallow";
+import useAuthStore from "../../hooks/auth-store";
+import { select } from "../../lib/utils/select";
+import { build } from "../../lib/api";
+import LoaderIcon from "../icons/loader-icon";
+import { useState } from "react";
 
-const ControlPanel: React.FC = ({}) => {
-  const validTest = useEditorStore((state) => {
+const ControlPanel: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const serviceConfig = useAuthStore(select("host", "credentials"), shallow);
+  const { validTest, currentTabId } = useEditorStore((state) => {
     const tab = state.tabs[state.currentTabId];
 
-    return validate(tab.buffer, getLanguage(tab.extension).linters);
-  });
+    return {
+      validTest: validate(tab.buffer, getLanguage(tab.extension).linters),
+      currentTabId: state.currentTabId,
+    };
+  }, shallow);
 
-  const currentTabId = useEditorStore((state) => state.currentTabId);
-  const [overlayVisible, setOverlayVisible] = useState(false);
+  const { write, showIndicator, hideIndicator } = useTerminalStore(
+    select("write", "showIndicator", "hideIndicator")
+  );
+
+  const handleBuild = async () => {
+    try {
+      setLoading(true);
+      showIndicator("Building...");
+      const results = await build(currentTabId, serviceConfig);
+      write(<pre>{JSON.stringify(results, null, 2)}</pre>);
+    } catch (e) {
+    } finally {
+      setLoading(false);
+      hideIndicator();
+    }
+  };
+
   return (
     <div className={styles.controlPanel}>
-      <button disabled={!validTest} className={styles.test}>
-        <PlayIcon className={styles.playIcon} />
-        <span>Test</span>
+      <button
+        onClick={handleBuild}
+        disabled={!validTest || loading}
+        className={styles.test}
+      >
+        {loading ? <LoaderIcon className={styles.loaderIcon} /> : <PlayIcon />}
+        <span>Build</span>
       </button>
-      <div className={styles.deployContainer}>
-        <button
-          className={styles.deploy}
-          onClick={() => setOverlayVisible(!overlayVisible)}
-        >
-          <span>Deploy</span>
-          <ChevronIcon
-            className={classNames(styles.chevronIcon, {
-              [styles.activeChevron]: overlayVisible === true,
-            })}
-          />
-        </button>
-        {overlayVisible ? (
-          <div className={styles.overlay}>
-            <div className={styles.headline}>
-              <span className={styles.target}>Deploy on</span>
-              <VariantIcon
-                platform={parseVariant(currentTabId)?.platform}
-                className={styles.platformIcon}
-              />
-              <HelpIcon className={styles.helpIcon} />
-            </div>
-            <span className={styles.description}>
-              Use curl or wget to deploy the Detect node on your instance.
-            </span>
-            <div className={styles.download}>
-              <div className={styles.link}>This is the linkja jajjajaa</div>
-              <div className={styles.copyContainer}>
-                <CopyIcon className={styles.copyIcon} />
-              </div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
     </div>
   );
 };
