abf4de949c74196b67543fbc75cba45f44c0e43c
Kenrick Beckett <kenrick.beckett@gmail.com>
2022-11-24 19:30:35 -0700

    ds: add Accordion component

---
+++ src/components/ds/accordion/accordion.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+import ChevronIcon from "../../icons/chevron-icon";
+import { Loading } from "../../icons/loading";
+import styles from "./accordion.module.css";
+
+const Accordion: React.FC<{
+  title: string;
+  children?: JSX.Element | JSX.Element[];
+  loading?: boolean;
+  expanded: boolean;
+  onToggle: () => void;
+}> = ({ title, children, expanded, loading, onToggle }) => {
+  return (
+    <div
+      className={classNames(styles.accordion, {
+        [styles.active]: expanded,
+      })}
+    >
+      <header onClick={onToggle}>
+        <span>{title}</span>
+        {loading ? <Loading /> : <ChevronIcon className={styles.expand} />}
+      </header>
+      {expanded && children}
+    </div>
+  );
+};
+
+export default Accordion;
