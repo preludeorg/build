14be81e5f9c2fbd9fd2849634ffb771be39ecab0
Kenrick Beckett <kenrick.beckett@gmail.com>
2022-12-02 07:35:22 -0700

    Hide create and delete variant commands on prelude tests

--- apps/build/src/lib/commands/helpers.tsx
+++ apps/build/src/lib/commands/helpers.tsx
@@ -1,8 +1,14 @@
 import { authState } from "../../hooks/auth-store";
 import { terminalState } from "../../hooks/terminal-store";
+import { isPreludeTest } from "../api";
 
 export const isConnected = () => !!authState().credentials;
 export const isInTestContext = () => !!terminalState().currentTest;
+
+export const isPreludeTestContext = () => {
+  const currentTest = terminalState().currentTest;
+  return currentTest && isPreludeTest(currentTest);
+};
 
 export const isExitError = (e: unknown) => {
   if (!(e instanceof Error)) {
