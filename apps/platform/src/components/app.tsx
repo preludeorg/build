7dc96f678609a6405486bc6f4048fa026f0f88b5
stephanwampouille <stephan.wampouille@prelude.org>
2022-12-06 12:07:11 -0500

    using createBrowserRouter

--- apps/platform/src/components/app.tsx
+++ apps/platform/src/components/app.tsx
@@ -1,23 +1,32 @@
 import Build from "@theprelude/build";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./header/header";
 import Nav from "./nav/nav";
 import styles from "./platform.module.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Build />,
+    errorElement: <Build />,
+    children: [
+      {
+        path: "/build",
+        element: <Build />,
+      },
+    ],
+  },
+]);
+
 function Platform() {
   return (
-    <Router>
-      <div className={styles.platform}>
-        <Header />
-        <Nav />
-        <main className={styles.wrapper}>
-          <Routes>
-            <Route path="/" element={<Build />} />
-            <Route path="/build" element={<Build />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className={styles.platform}>
+      <Header />
+      <Nav />
+      <main className={styles.wrapper}>
+        <RouterProvider router={router} />
+      </main>
+    </div>
   );
 }
 
--- apps/platform/src/components/nav/nav.tsx
+++ apps/platform/src/components/nav/nav.tsx
@@ -1,6 +1,5 @@
 import { BuildIcon } from "@theprelude/ds";
 import classNames from "classnames/bind";
-import { Link } from "react-router-dom";
 import styles from "./nav.module.css";
 
 const cx = classNames.bind(styles);
@@ -9,12 +8,12 @@ const Nav: React.FC = () => {
   return (
     <nav className={cx("nav")}>
       <section className={cx("top")}>
-        <Link to="/build" className={cx("item", { active: true })}>
+        <a href="/" className={cx("item", { active: true })}>
           <BuildIcon />
-        </Link>
-        <Link to="/build" className={cx("item", { active: false })}>
+        </a>
+        <a href="/build" className={cx("item", { active: false })}>
           <BuildIcon />
-        </Link>
+        </a>
       </section>
     </nav>
   );
