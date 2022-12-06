b5acc71de2097dabc1f3ff77bce21738575e4592
stephanwampouille <stephan.wampouille@prelude.org>
2022-12-06 15:39:02 -0500

    feat: adding user dropdown menu

--- apps/platform/src/components/header/header.tsx
+++ apps/platform/src/components/header/header.tsx
@@ -1,7 +1,35 @@
-import { PreludeIcon } from "@theprelude/ds";
+import {
+  CheckmarkIcon,
+  ChevronIcon,
+  CloseIcon,
+  IconButton,
+  PreludeIcon,
+  UserIcon,
+} from "@theprelude/ds";
+import { useState } from "react";
 import styles from "./header.module.css";
 
 const Header: React.FC = () => {
+  const [optionsVisible, setOptionsVisible] = useState(false);
+  const [managerVisible, setManagerVisible] = useState(false);
+
+  const handleManager = () => {
+    setOptionsVisible(false);
+    setManagerVisible(true);
+  };
+
+  const handleClose: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+    setOptionsVisible(false);
+    setManagerVisible(false);
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const handle = formData.get("handle") as string;
+  };
+
   return (
     <header className={styles.header}>
       <section className={styles.brand}>
@@ -9,7 +37,58 @@ const Header: React.FC = () => {
         <span className={styles.divider} />
         <h1>Build</h1>
       </section>
-      <section className={styles.right}></section>
+      <section className={styles.right}>
+        <div
+          onClick={() => setOptionsVisible(!optionsVisible)}
+          className={styles.account}
+        >
+          <UserIcon />
+          <span>anonymous-user-23231</span>
+          <ChevronIcon
+            className={
+              optionsVisible === true || managerVisible === true
+                ? styles.activeChevron
+                : ""
+            }
+          />
+          {optionsVisible && (
+            <div
+              className={styles.options}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <span onClick={handleManager}>Create a handle</span>
+              <div className={styles.divider} />
+              <span>Import credentials</span>
+              <span>Export credentials</span>
+            </div>
+          )}
+          {managerVisible && (
+            <div className={styles.create}>
+              <div className={styles.title}>
+                <span>Your Prelude Handle</span>
+                <IconButton
+                  className={styles.close}
+                  icon={<CloseIcon />}
+                  onClick={handleClose}
+                />
+              </div>
+              <div className={styles.divider} />
+              <p>
+                You can change your handle at any time to access the tests you
+                create and run.
+              </p>
+              <form onSubmit={handleSubmit}>
+                <input type="text" name={"handle"}></input>
+                <IconButton
+                  type="submit"
+                  icon={<CheckmarkIcon />}
+                  intent="secondary"
+                />
+              </form>
+            </div>
+          )}
+        </div>
+      </section>
     </header>
   );
 };
