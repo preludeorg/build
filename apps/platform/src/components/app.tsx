da34936a3bb3668bd6ae74888611e81a359d940d
stephanwampouille <stephan.wampouille@prelude.org>
2022-12-06 11:30:17 -0500

    feat: adding React Router

--- apps/platform/src/components/app.tsx
+++ apps/platform/src/components/app.tsx
@@ -1,17 +1,23 @@
 import Build from "@theprelude/build";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./header/header";
 import Nav from "./nav/nav";
 import styles from "./platform.module.css";
 
 function Platform() {
   return (
-    <div className={styles.platform}>
-      <Header />
-      <Nav />
-      <main className={styles.wrapper}>
-        <Build />
-      </main>
-    </div>
+    <Router>
+      <div className={styles.platform}>
+        <Header />
+        <Nav />
+        <main className={styles.wrapper}>
+          <Routes>
+            <Route path="/" element={<Build />} />
+            <Route path="/build" element={<Build />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- apps/platform/src/components/nav/nav.tsx
+++ apps/platform/src/components/nav/nav.tsx
@@ -1,5 +1,6 @@
 import { BuildIcon } from "@theprelude/ds";
 import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
 import styles from "./nav.module.css";
 
 const cx = classNames.bind(styles);
@@ -8,9 +9,11 @@ const Nav: React.FC = () => {
   return (
     <nav className={cx("nav")}>
       <section className={cx("top")}>
-        <a className={cx("item", { active: true })}>
-          <BuildIcon />
-        </a>
+        <Link to="/build">
+          <a className={cx("item", { active: true })}>
+            <BuildIcon />
+          </a>
+        </Link>
       </section>
     </nav>
   );
