984ff1a60c82880e85d7922d10e572bf1e62f115
Kenrick Beckett <760470+kenrick@users.noreply.github.com>
2022-12-06 03:58:20 -0700

    refactor: move stylesheets to ds package (#215)

--- apps/platform/src/main.tsx
+++ apps/platform/src/main.tsx
@@ -1,7 +1,7 @@
+import "@theprelude/ds/system.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app";
-import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
