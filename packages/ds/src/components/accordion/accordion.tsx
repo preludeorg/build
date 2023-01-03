1c192b92f20af0c71170a2cc7641e4e15dc359cc
Alex Manners <alexandermanners@gmail.com>
2023-01-03 14:03:31 -0500

    edit button is available

--- packages/ds/src/components/accordion/accordion.tsx
+++ packages/ds/src/components/accordion/accordion.tsx
@@ -2,24 +2,29 @@ import classNames from "classnames";
 import { ChevronIcon } from "../icons/chevron-icon";
 import { Loading } from "../icons/loading";
 import styles from "./accordion.module.css";
+import React from "react";
 
 export const Accordion: React.FC<{
   title: React.ReactNode;
   children?: JSX.Element | JSX.Element[];
   loading?: boolean;
+  edit?: React.ReactNode;
   expanded: boolean;
   onToggle: () => void;
-}> = ({ title, children, expanded, loading, onToggle }) => {
+}> = ({ title, children, expanded, edit, loading, onToggle }) => {
   return (
     <div
       className={classNames(styles.accordion, {
         [styles.active]: expanded,
       })}
     >
-      <header onClick={onToggle}>
-        <div>{title}</div>
-        {loading ? <Loading /> : <ChevronIcon className={styles.expand} />}
-      </header>
+      <div>
+        <header onClick={onToggle}>
+          <div>{title}</div>
+          {loading ? <Loading /> : <ChevronIcon className={styles.expand} />}
+        </header>
+        {edit && <>{edit}</>}
+      </div>
       {expanded && children}
     </div>
   );
