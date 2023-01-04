73b4457e26fece0a55fea85b6c7c847e98d25cef
Alex <43142871+khyberspache@users.noreply.github.com>
2023-01-04 10:30:40 -0500

    fix border and remove variant parsing for tests/code (#268)

--- packages/core/src/utils/parse-variant.ts
+++ packages/core/src/utils/parse-variant.ts
@@ -1,31 +1,11 @@
-const VST_FORMAT =
-  /([a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})(_(\w+))?(-(\w+))?\.(\w+)/i;
-
 const BUILD_VST_FORMAT =
   /([a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})(_(\w+))(-(\w+))(\.\w+)?/i;
-
-interface ParsedVST {
-  vst: string;
-  id: string;
-  platform?: string;
-  arch?: string;
-  language: string;
-}
 
 interface ParsedBuildVST {
   vst: string;
   id: string;
   platform: string;
   arch: string;
-}
-
-export function parseVerifiedSecurityTest(vstName: string): ParsedVST | null {
-  const results = vstName.match(VST_FORMAT);
-  if (!results) {
-    return null;
-  }
-  const [vst, id, , platform, , arch, language] = results;
-  return { vst, id, platform, arch, language };
 }
 
 export function parseBuildVerifiedSecurityTest(
