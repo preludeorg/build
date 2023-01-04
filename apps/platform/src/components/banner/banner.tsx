2f3c1c08a7ed9902aaff66132046ce1010fb3244
Alex <43142871+khyberspache@users.noreply.github.com>
2023-01-04 14:49:38 -0500

    adding a header banner (#270)
    Co-authored-by: Kenrick Beckett <kenrick.beckett@gmail.com>

---
+++ apps/platform/src/components/banner/banner.tsx
@@ -0,0 +1,24 @@
+import { select, useAuthStore } from "@theprelude/core";
+import { CloseIcon, IconButton } from "@theprelude/ds";
+import React from "react";
+import styles from "./banner.module.css";
+
+const Banner: React.FC = () => {
+  const [hide, setHide] = React.useState(false);
+  const { isAnonymous } = useAuthStore(select("isAnonymous"));
+
+  if (!isAnonymous || hide) return null;
+
+  return (
+    <div className={styles.banner}>
+      <span className={styles.message}>
+        Register an account using the account drop-down on the right to save
+        your work.
+      </span>
+
+      <IconButton icon={<CloseIcon />} onClick={() => setHide(true)} />
+    </div>
+  );
+};
+
+export default Banner;
